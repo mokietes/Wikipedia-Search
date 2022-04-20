698f483c9311b67ff191a7933be40f544244c32c
abc <abc@example.com>
2022-04-20 15:08:03 -0700

    new message

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputValue = document.querySelector(".js-search-input").value;
+  const searchQuery = inputValue.trim();
+  console.log(searchQuery);
+}
+
 const form = document.querySelector(".js-search-form");
+form.addEventListener("submit", handleSubmit);
